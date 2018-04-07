5ab32e90237acb4d6ff7a083f14c6c0cb1765e49
Stefan Kowal <kowalmax.s@gmail.com>
2018-04-07 16:29:52 -0600

    uh huh

--- .env/.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	spotify_app_id: "a8e34050570947dfb788ed4bfd87616d",
-	spotify_app_secret: "6b9cd1a845e74fbd8e2a0dbecb2f2e52",
-	spotify_redirect_url: "http://localhost:3000/api/auth/spotify_redirect",
-	domain: "http://localhost:3000"
-}
