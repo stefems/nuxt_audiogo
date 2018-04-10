06a68d1c8f5f3657f200ad9ac8085444d2a51fdf
Stefan Kowal <kowalmax.s@gmail.com>
2018-04-09 22:30:49 -0600

    got the streamer working for just the artist

--- pages/discover.vue
+++ pages/discover.vue
@@ -1,12 +1,20 @@
 <template>
   <section class="container">
-   <h1>Discover</h1>
+   <h1>
+	   <!-- v-click="set_artist_bubbles($store.state.user.top_artists)" -->
+	   <span class="nav-text">reload favorites</span>
+	   <span>{{title}}</span>
+	</h1>
 
-   <div class="artist-bubble" v-for="bubble in artist_bubbles" :key="bubble.id" v-bind:style="{ backgroundImage: 'url(' + bubble.url + ')' }">
-	   <span>{{bubble.name}}</span>
+   <div v-on:click="open_stream(bubble)" class="artist-bubble" v-for="bubble in artist_bubbles" :key="bubble.id" v-bind:style="{ backgroundImage: 'url(' + bubble.url + ')' }">
+	   <span v-bind:id="bubble.id">{{bubble.name}}</span>
 	</div>
 
-   <!-- <div class="artist-bubble" v-bind:style="{ backgroundImage: 'url(' + artist_images[2] + ')' }"></div> -->
+	<div class="player" v-if="show_player">
+		<h4 v-if="device_id != ''">Player Ready</h4>
+		<script src="https://sdk.scdn.co/spotify-player.js"></script>
+	</div>
+
   </section>
 </template>
 
@@ -18,14 +26,50 @@ import spotify_endpoints from "../services/spotify_endpoints.js";
 export default {
 	data() {
 		return {
-			artist_bubbles: []
+			artist_bubbles: [],
+			token: '',
+			title: 'Root',
+			show_player: false,
+			device_id: ''
 		}
 	},
 	methods: {
+		open_stream: function(bubble) {
+			this.artist_bubbles = [bubble];
+			this.title = "Tree";
+			this.show_player = true;
+			window.onSpotifyWebPlaybackSDKReady = () => {
+				const token = this.token; //'[My Spotify Web API access token]';
+				const player = new Spotify.Player({
+					name: 'Web Playback SDK Quick Start Player',
+					getOAuthToken: cb => {
 
-	},
-	beforeMount() {
-		this.set_artist_bubbles = function(top_artists) {
+						cb(token);
+					}
+				});
+
+				// Error handling
+				player.addListener('initialization_error', ({ message }) => { console.error(message); });
+				player.addListener('authentication_error', ({ message }) => { console.error(message); });
+				player.addListener('account_error', ({ message }) => { console.error(message); });
+				player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+				// Playback status updates
+				player.addListener('player_state_changed', state => { console.log(state); });
+
+				// Ready
+				player.addListener('ready', function({ device_id }) {
+					this.device_id = device_id;
+					spotify_endpoints.get_song(this.artist_bubbles[0].id, this.token).then( function(track_id) {
+						spotify_endpoints.set_track(track_id, this.token, this.device_id);
+					}.bind(this));
+				}.bind(this));
+
+				// Connect to the player!
+				player.connect();
+			};
+		},
+		set_artist_bubbles: function(top_artists) {
 			let artists_chosen = [];
 			this.randomProperty = function(obj) {
 				var keys = Object.keys(obj)
@@ -48,7 +92,7 @@ export default {
 			this.load_all_artists = function(artists) {
 				let artist_promises = [];
 				artists.forEach( (artist) => {
-					artist_promises.push(spotify_endpoints.get_artist_image(artist.id, token));
+					artist_promises.push(spotify_endpoints.get_artist_image(artist.id, this.token));
 				});
 
 				this.handle_all = function(results) {
@@ -62,11 +106,12 @@ export default {
 			
 			this.load_all_artists(artists_chosen);
 		}
-		var token = '';
+	},
+	beforeMount() {
 		var images = this.artist_images;
 		if (sessionStorage.getItem("showgo_user") !== "undefined" && sessionStorage.getItem("showgo_user") !== null) {
 			this.$store.commit('store_user', JSON.parse(sessionStorage.getItem("showgo_user")));
-			token = JSON.parse(sessionStorage.getItem("showgo_user")).spotify_access_token;
+			this.token = JSON.parse(sessionStorage.getItem("showgo_user")).spotify_access_token;
 			this.set_artist_bubbles(this.$store.state.user.top_artists);
 		} else {
 			console.log("dont have user");
@@ -77,6 +122,14 @@ export default {
 </script>
 
 <style scoped>
+.nav-text {
+	font-size: 10px;
+    vertical-align: middle;
+    position: absolute;
+    left: 15px;
+    cursor: pointer;
+    text-decoration: underline;
+}
 .artist-bubble {
 	width: 150px;
 	height: 150px;
@@ -88,6 +141,7 @@ export default {
 	font-size: 20px;
 	text-align: center;
 	font-family: verdana, sans-serif;
+	cursor: pointer;
 }
 .artist-bubble span {
 	color: white;
--- services/spotify_endpoints.js
+++ services/spotify_endpoints.js
@@ -15,5 +15,31 @@ export default {
 		});
 
 		return artist_image_promise;
+	},
+	get_song: function(artist_id, token) {
+		axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+
+		let artist_track_promise = new Promise( (resolve) => {
+			axios.get("https://api.spotify.com/v1/artists/" + artist_id + "/top-tracks?country=US").then( (result) => {
+				let track_id = result.data.tracks[0].uri;
+				resolve(track_id);
+			});
+		});
+
+		return artist_track_promise;
+	},
+	set_track: function(track_id, token, device_id) {
+		axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+		let body = {
+			uris: [track_id]
+		}
+		let play_track_promise = new Promise( (resolve) => {
+			axios.put("https://api.spotify.com/v1/me/player/play?device_id=" + device_id, body).then( (result) => {
+				// let track_id = result.data.tracks[0].id;
+				// resolve(track_id);
+			});
+		});
+
+		return play_track_promise;
 	}
 }
